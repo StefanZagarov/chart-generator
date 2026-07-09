6bffdfaeccb428ef8d7ddbf4d9ea26959cdb9af7
StefanZagarov <stefanzagarov@gmail.com>
2026-07-09 22:23:27 +0300

    Initial setup: Vite + React 19 + TS, Tailwind v4, fonts, and palette

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-svh">
+      <h1 className="">Natal Chart</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "@fontsource/eb-garamond/400.css";
+import "@fontsource/eb-garamond/400-italic.css";
+import "@fontsource/eb-garamond/500.css";
+import "@fontsource/eb-garamond/600.css";
+import "@fontsource/im-fell-english";
+import "@fontsource/im-fell-english/400-italic.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
